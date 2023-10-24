82514c67fd5178d5ef050da14049a4fddc94a794
Streikin <109292425+Streikin@users.noreply.github.com>
2023-10-24 18:46:35 +0200

    Update vocabulary_trainer.py

--- vocabulary trainer.py
+++ vocabulary trainer.py
@@ -1,23 +1,13 @@
 import random
 
-def colorize(text, color):
-  colors = {
-  'red': '\033[91m',
-  'green': '\033[92m',
-  'blue': '\033[94m'
-   }
-  return colors[color] + text + '\033[0m'
-
 #set your vocabulary here
 vocabulary = {"House":"Haus","Apple":"pomme","Man":"homme"}
 
 while True:
   language1, language2 = random.choice(list(vocabulary.items()))
   #change to the correct language
-  print("What is the --language-- word for " + colorize(language1, 'blue') + "?")
-  answer = input("> ")
+  answer = input(f"What is the --language-- word for \033[94m{language1}\033[0m?\n> ")
   if answer.lower() == language2.lower():
-    print(colorize("Correct!", 'green'))
+    print(f"\033[92mCorrect!\033[0m")
   else:
-    print(colorize("Incorrect!", 'red'))
-    print("The correct answer is: " + colorize(language2, 'blue'))
+    print(f"\033[91mIncorrect!\033[0m\nThe correct answer is: \033[94m{language2}\033[0m.")
